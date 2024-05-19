b9b37802132a61d2623824b410e473692aa8ac59
abg <softabg@gmail.com>
2024-05-19 20:17:24 +0200

    primera aproximación a versión móvil de la portada.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      'verdoso': 'color(display-p3 .95 .95 .9)',
+      'azul': 'rgb(13,110,253)'
+    },
     extend: {},
   },
   plugins: [],
